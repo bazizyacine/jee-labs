4e73b4b8a461e515483f32e240a3e830e186d568
bazizyacine <58179990+bazizyacine@users.noreply.github.com>
2020-01-02 14:43:25 +0100

    test

---
+++ TestHttpServlet.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map.Entry;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Administrateur
+ */
+@WebServlet(urlPatterns = {"/Test"})
+public class TestHttpServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Création d’une Servlet</title>");
+            out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"index.css\"/>");
+            out.println("</head>");
+           
+            out.println("<body>");
+            out.println("<h1>Méthode d'envoie de la requête :<br><br>" +  request.getMethod() + "</h1>");
+            
+            out.println("<h1>Paramètres de la requête :</h1>");
+            
+            out.println("<table>");
+            
+            out.println("<tr><th>Nom</th><th>Valeur</th></tr>");
+
+            for(Entry<String, String[]> entry : request.getParameterMap().entrySet()) {
+                
+                if(entry.getValue() != null && entry.getValue().length > 0) { 
+                   out.println("<tr><td>" + entry.getKey()+ "</td><td>" + entry.getValue()[0]);
+                }
+            }
+            
+            out.println("</table>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
